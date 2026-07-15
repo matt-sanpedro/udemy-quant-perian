f36427481b53b1c906f7dbf445b3804bee53d330
matt-sanpedro <mattsanpedro7@gmail.com>
2026-07-14 21:58:24 -0700

    Enhanced while loops with print statements and end clause

--- section-01-python-review/p-06-for-loops.py
+++ section-01-python-review/p-06-for-loops.py
@@ -1,16 +1,17 @@
 # for loop using the range function
 for i in range(5):
-    print(i)
+    print(i, end=" ")
 
 # changing the increments with the range function
+print("\n")
 for i in range(0,20,2):
-    print(i)
+    print(i, end=" ")
 
 # appending to a list
 x = []
 for i in range(5):
     x.append(i)
-print(x)
+print("\n{}".format(x))
 
 # list comprehension
 print([num**2 for num in x])
