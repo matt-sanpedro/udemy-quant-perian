145dcb0b870bc20d5229685c54983892026cc408
matt-sanpedro <mattsanpedro7@gmail.com>
2026-07-20 04:54:39 -0700

    Added introductory notes to the pandas dataframe object

---
+++ section-03-pandas/p-02-dataframe-intro.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+"""
+dataframe: a group of pandas series objects that share the same index
+    - technically, it is a two-dimensional (size-mutable) tabular data structure
+    - contains labeled axes (rows and columns)
+"""
+
+# create a dataframe from Python objects
