25a42240958359461e872f0f12823736539322ed
matt-sanpedro <mattsanpedro7@gmail.com>
2026-07-20 18:30:47 -0700

    Completed pandas df exercise including initializing a df from numpy arrays and reading a csv file regardless of the operating system - path separator dependent

--- section-03-pandas/p-02-dataframe-intro.py
+++ section-03-pandas/p-02-dataframe-intro.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import os
 
 """
 dataframe: a group of pandas series objects that share the same index
@@ -8,3 +9,20 @@ dataframe: a group of pandas series objects that share the same index
 """
 
 # create a dataframe from Python objects
+np.random.seed(101)
+# note that the high number is exclusive
+mydata = np.random.randint(0, 101, (4,3))
+print(mydata)
+
+myindex = ['CA', 'NY', 'AZ', 'TX']
+mycolumns = ['Jan', 'Feb', 'Mar']
+df = pd.DataFrame(data=mydata, index=myindex, columns=mycolumns)
+print(df)
+
+# info method
+df.info()
+
+# read the csv file
+# robust way to read files: prevents issues with path separators
+df_csv = pd.read_csv(os.path.join(os.path.dirname(__file__), 'tips.csv'))
+print(df_csv)
