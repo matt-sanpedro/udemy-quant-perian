fd34ab9b69782b161dd97a940fed3e09a97d8923
matt-sanpedro <mattsanpedro7@gmail.com>
2026-07-31 22:20:02 -0700

    Added print statements for clarity

--- section-03-pandas/p-03-dataframe-properties.py
+++ section-03-pandas/p-03-dataframe-properties.py
@@ -7,7 +7,7 @@ df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'tips.csv'))
 print(df)
 
 # list the columns in a dataframe
-print(df.columns)
+print('Calling columns method to list the columns: {}'.format(df.columns))
 
 # returns boolean if column exists in df
 print('total_bill' in df.columns)
@@ -17,19 +17,20 @@ for col in df.columns:
     print(col)
 
 # extract indices
-print(df.index)
+print('Calling index method to list the indices: {}'.format(df.index))
 
 # print first couple of rows
-print(df.head(10))
+print(df.head(3))
 
 # print last couple of rows
-print(df.tail(20))
+print(df.tail(5))
 
 # get the df info
+print('\nInfo on DF:')
 df.info()
 
 # for numeric columns, calculate statistics
-print(df.describe())
+print('Calling the describe method:\n{}'.format(df.describe()))
 
 # can also call transpose on the describe method
-print(df.describe().transpose())
+print('Transposing the describe statistics:\n{}'.format(df.describe().transpose()))
