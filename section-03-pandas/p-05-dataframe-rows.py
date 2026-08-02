5c6d1c628564f0aa1013b03952f62249e6d66abd
matt-sanpedro <mattsanpedro7@gmail.com>
2026-08-01 19:42:01 -0700

    Corrected check_duplicate function on since isna() must be invoked on the dataframe specified column

--- section-03-pandas/p-05-dataframe-rows.py
+++ section-03-pandas/p-05-dataframe-rows.py
@@ -45,7 +45,7 @@ def check_duplicates(df: pd.DataFrame, index_col: str):
         All values in the column have no duplicates or NaN values
     """
     has_dup = df[df[index_col].duplicated()].shape[0] > 0
-    has_nan = df.isna().any().any()
+    has_nan = df[index_col].isna().any().any()
     return has_dup and not has_nan
 
 print('Check Duplicate: {}'.format(check_duplicates(df, index_col)))
