0f229cf05c43ddaf410d28521b4eb1c166b4fbc7
matt-sanpedro <mattsanpedro7@gmail.com>
2026-08-12 22:02:54 -0700

    Completed more exercises up to 10 largest companies by market cap

--- section-03-pandas/io/q-00-io.py
+++ section-03-pandas/io/q-00-io.py
@@ -7,6 +7,28 @@ def get_col_datatype(df):
         col_dict[col] = df[col].dtype
     return col_dict
 
+def check_duplicates(df: pd.DataFrame, index_col: str):
+    """
+    Determines if the given column has duplicate or NaN values
+        
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The source DataFrame to inspect.
+    index_col : str
+        The column name intended to become the future index.
+
+    Returns
+    -------
+    True
+        Duplicate or NaN values exists
+    False:
+        All values in the column have no duplicates or NaN values
+    """
+    has_dup = df[df[index_col].duplicated()].shape[0] > 0
+    has_nan = df[index_col].isna().any().any()
+    return has_dup or has_nan
+
 # Read the two datasets provided in the current directory (also available in the DATA folder. 
 # (The file names are: constituents.csv and constituents-financials.csv)
 df_cons = pd.read_csv(os.path.join(os.path.dirname(__file__), 'constituents.csv'))
@@ -26,3 +48,22 @@ print(get_col_datatype(df_cons))
 print(get_col_datatype(df_fin))
 print(df_cons.dtypes)
 print(df_fin.dtypes)
+
+# Print the first 5 rows of both datasets**
+print('First 5 rows df_cons:\n{}'.format(df_cons.head(5)))
+print('First 5 rows df_fin:\n{}'.format(df_fin.head(5)))
+
+# Task: Drop the SEC Filings column**
+df_fin = df_fin.drop('SEC Filings',axis=1)
+print('First 5 rows df_fin:\n{}'.format(df_fin.head(5)))
+
+# Task: Set the Symbol column to the index in the financials dataset**
+print('Symbol row duplicates exist: {}'.format(check_duplicates(df_fin, 'Symbol')))
+df_fin = df_fin.set_index('Symbol')
+print('First 5 rows df_fin:\n{}'.format(df_fin.head(5)))
+
+# Task: What are the 10 largest companies according to market cap?**
+print('\nNLARGEST\nData type: {}\nData:\n{}'.format(type(df_fin['Market Cap'].nlargest(10)), df_fin['Market Cap'].nlargest(10)))
+
+# Task: Return all information about the 10 largest companies according by market cap**
+print('\n10 largest companies according to market cap:\n{}'.format(df_fin.nlargest(10,'Market Cap')))
