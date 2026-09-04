1fa1d11ae8572021a256ee0f30532db1ac158b94
matt-sanpedro <mattsanpedro7@gmail.com>
2026-09-04 00:16:09 -0700

    Completed plotting exercises with slicing dataframes, setting x and y limits, and use of the locator and formatter methods

--- section-05-pandas-finance/p-02-pandas-time-series.py
+++ section-05-pandas-finance/p-02-pandas-time-series.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+from matplotlib import dates
 
 # resolve the csv path
 csv_path = os.path.abspath(
@@ -10,5 +11,25 @@ df = pd.read_csv(csv_path)
 df['Date'] = pd.to_datetime(df['Date'])
 df = df.set_index('Date',verify_integrity=True)
 print(df.index)
-df['Adj Close'].plot()
+
+print(df['Adj Close']['2018-01-01':'2020-01-01'])
+
+# SOLUTION 1: can slice a dataframe
+df['Adj Close']['2018-01-01':'2020-01-01'].plot()
 plt.show()
+
+# SOLUTION 2: can call the xlim method
+df['Adj Close'].plot(xlim=['2018-01-01','2020-01-01'],ylim=[150,300])
+plt.show()
+
+plt.figure(dpi=200)
+ax = df['Close']['2018-01-01':'2018-03-01'].plot()
+# ax.xaxis.set_major_locator(dates.WeekdayLocator(byweekday=0))
+
+# LOCATORS: set axes location of the ticks to a particular date position
+ax.xaxis.set_major_locator(dates.MonthLocator())
+# FORMATTER: format actual time stamp to string
+ax.xaxis.set_major_formatter(dates.DateFormatter('%Y-%B'))
+plt.show()
+
+ 
