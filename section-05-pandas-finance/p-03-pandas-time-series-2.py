6b531f467a82e0f3a55bbc88fc621fb0a9da6fe1
matt-sanpedro <mattsanpedro7@gmail.com>
2026-09-07 22:24:55 -0700

    Completed exercise on more advanced locator and formatter methods

---
+++ section-05-pandas-finance/p-03-pandas-time-series-2.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+from matplotlib import dates
+
+# resolve the csv path
+csv_path = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", "DATA", "COST.csv")
+)
+df = pd.read_csv(csv_path)
+df['Date'] = pd.to_datetime(df['Date'])
+df = df.set_index('Date',verify_integrity=True)
+
+plt.figure(figsize=(8,6),dpi=200)
+ax = df['Close']['2018-01-01':'2020-01-01'].plot()
+ax.xaxis.set_major_locator(dates.YearLocator())
+ax.xaxis.set_major_formatter(dates.DateFormatter('%Y      %B'))
+ax.xaxis.set_minor_locator(dates.MonthLocator())
+ax.xaxis.set_minor_formatter(dates.DateFormatter('%B'))
+ax.tick_params(axis='x',which='minor',rotation=90)
+ax.tick_params(axis='x',which='major',rotation=90)
+plt.xticks(ha='center')
+ax.grid(True,axis='both',which='both')
+plt.tight_layout()
+plt.show()
