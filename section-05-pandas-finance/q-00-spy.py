5da15316b483a8785d62ab42fa964bac543e1faa
matt-sanpedro <mattsanpedro7@gmail.com>
2026-09-20 23:34:39 -0700

    Added additional solutions

--- section-05-pandas-finance/q-00-spy.py
+++ section-05-pandas-finance/q-00-spy.py
@@ -23,30 +23,32 @@ ax.tick_params(axis='x',which='major',rotation=90)
 plt.xticks(ha='center')
 plt.tight_layout()
 plt.savefig('sp500.png',bbox_inches='tight')
-# plt.show()
+plt.show()
 
 # **Task: Create a histogram of the daily volume of shares of SPY traded. Choose a reasonable bin size.**
-df['Volume'].plot(kind='hist',bins=50)
-# plt.show()
+# df['Volume'].plot(kind='hist',bins=50)
+df['Volume'].hist(bins=50)
+plt.show()
 
 # **Task: Let's explore the crash and recovery of 2020 due to the COVID pandemic. Create a line plot of the Adj. Close price from 1-1-2020 to 1-1-2021. The xaxis ticks should be formatted by YYYY-MM**
 plt.figure(figsize=(8,6),dpi=200)
 df['Adj Close'].plot(xlim=['2020-01-01','2021-01-01'])
-# plt.show()
+plt.show()
 
 # **Task: Create a plot whichs shows the adjusted closing price of SPY for the entire length of the time series along with an added trend line of the corresponding 200 days rolling mean of the adj. close price. Make sure to add a legend that identifies each line.** 
 plt.figure(figsize=(10,4),dpi=200)
 ax_adj = df['Adj Close'].plot(label='Adj Close')
 df['Adj Close'].rolling(window=200).mean().plot(lw=0.8,label='200 MA of Adj Close')
 ax_adj.legend()
-# plt.show()
+plt.show()
 
 '''
 **Task: If you bought one share at the start of the time series on Jan 1st 2000, how much money would you have gained (in Adj. Close Dollar amount) by Jan 1st 2021?**
 
 Take the adjusted closing price as reference
 '''
-gains = df['Adj Close']['2020-12-31'] - df['Adj Close']['2000-01-03']
+# gains = df['Adj Close']['2020-12-31'] - df['Adj Close']['2000-01-03']
+gains = df['Adj Close'].iloc[-1] - df['Adj Close'].iloc[0]
 print('Gains from one share of SPY: {}'.format(gains))
 
 # **Task: What was the percent increase in value (based on Adj. Close price) from Jan 1st, 2000 to Jan 1st 2021?**
@@ -55,7 +57,8 @@ print('Percent increase from one share of SPY: {}'.format(percent_inc))
 
 # **Task: Return the 10 dates with the largest gain for the SPY.**
 df['Delta Adj Close'] = df['Adj Close'].pct_change(1)
-print('Largest gain for the SPY: {}'.format(df['Delta Adj Close'].nlargest(10))) 
+# print('Largest gain for the SPY: {}'.format(df['Delta Adj Close'].nlargest(10))) 
+print('Largest gain for the SPY: {}'.format(df['Delta Adj Close'].sort_values(ascending=False).head(10))) 
 
 # **TASK: What were the 10 dates with the highest daily percent change from the previous day (either gain OR decrease).**
 print(df['Delta Adj Close'].abs().nlargest(10))
@@ -70,9 +73,13 @@ print('Max Adj Close of SPY per year:\n{}'.format(annual_max))
 Hint: Check out idxmin()
 '''
 # print('Price of lowest adjusted closing: {}'.format(df['Adj Close']['2020-01-01':'2020-12-31'].min()))
-print('Price of lowest adjusted closing: {}'.format(df.loc['2020-01-01':'2020-12-31', 'Adj Close'].min()))
+# print('Price of lowest adjusted closing: {}'.format(df.loc['2020-01-01':'2020-12-31', 'Adj Close'].min()))
+print('Price of lowest adjusted closing: {}'.format(df.loc['2020', 'Adj Close'].min()))
+
 # print('Day with lowest adjusted closing: {}'.format(df['Adj Close']['2020-01-01':'2020-12-31'].idxmin()))
-print('Day with lowest adjusted closing: {}'.format(df.loc['2020-01-01':'2020-12-31', 'Adj Close'].idxmin()))
+# print('Day with lowest adjusted closing: {}'.format(df.loc['2020-01-01':'2020-12-31', 'Adj Close'].idxmin()))
+print('Day with lowest adjusted closing: {}'.format(df.loc['2020', 'Adj Close'].idxmin()))
+
 
 # **Task: In 2020, due to the uncertainty of the COVID-19 pandemic, the S&P500 index experienced a large drop in March of 2020. What was the highest price reached in 2020 *before* April 1st, 2020.**
 # high_index = df['Adj Close']['2020-01-01':'2020-03-31'].idxmax()
